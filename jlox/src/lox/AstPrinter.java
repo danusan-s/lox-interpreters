3b171235aaa4d83b683564be022c8453020c3525
danusan-s <danusansugumar@gmail.com>
2025-05-13 19:08:17 +0530

    Add variable and assign support for AstPrinter

--- jlox/src/lox/AstPrinter.java
+++ jlox/src/lox/AstPrinter.java
@@ -27,6 +27,16 @@ public class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr){
+        return " = " + expr.name.lexeme + " " + expr.value.accept(this);
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
